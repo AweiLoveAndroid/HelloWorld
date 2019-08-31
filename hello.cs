a15f05d002eff841b12fe06d341d824a3001c4c8
LZW <460433675@qq.com>
2019-08-31 19:12:33 +0800

    C# 语言

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
